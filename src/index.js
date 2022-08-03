c06cc2765ce2e2d2d4ae732702739db7706fc6d4
Hugo Rodríguez <hugsrodriguez8@gmail.com>
2022-08-03 17:04:59 -0600

    Agregando componentes con estilo

--- src/index.js
+++ src/index.js
@@ -5,30 +5,10 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import Card from "./components/Card";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const user = {
-    firstName : 'Hugo',
-    lastName : 'Rodríguez',
-    avatar : 'https://cdn-icons.flaticon.com/png/512/1144/premium/1144709.png?token=exp=1659484942~hmac=6f052108097fb3db38a2c67eeb828867',
-};
-
-function getName(user){
-    return `${user.firstName} ${user.lastName}`;
-}
-
-function getGreeting(user){
-    if(user){
-        return <h1>Hello {getName(user)}</h1>;
-    }
-    return <h1>Hello Stranger </h1>;
-}
-
-const element = (
-    <div>
-        {getGreeting(user)}
-        <img src={user.avatar} width="256" height="256" alt="userImage"/>
-    </div>);
 const container = document.getElementById('root');
 
 //ReactDOM.render(QUE,DONDE)
-ReactDOM.render(element, container)
+ReactDOM.render(<Card></Card>, container)
