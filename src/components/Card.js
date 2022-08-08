443c1f1c3a32afd2eb995a96e90fd0fddcb5a69f
Hugo Rodríguez <hugsrodriguez8@gmail.com>
2022-08-07 23:17:09 -0600

    Ultimos cambios

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,47 +3,24 @@ import circlesImg from "../images/circles.png"
 import emptyImg from "../images/empty.png";
 import "./styles/Card.css"
 
-class Card extends React.Component{
-
-    /*constructor(props){
-        super(props);
-        this.state = {
-            image: loadingImg
-        }
-    }
-
-    componentDidMount(){
-        setTimeout(() => {
-            this.setState({
-                image: this.props.img
-            });
-        }, 5000);
-    }*/
-
-    render(){
-        const { title, description, img, leftColor, rightColor } = this.props;
-
-        return (
-            <div className="card mx-auto Fitness-Card"
-            style={{
-                backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor || '#56CCF2'}, ${rightColor || '#2F80ED'})`
-            }}
-            >
-                <div className="card-body">
-                    <div className="row center">
-                        <div className="col-6">
-                            <img src={img || emptyImg} className="float-end" alt="img"/>
-                        </div>
-                        <div className="col-6 Fitness-Card-Info">
-                            <h1>{title}</h1>
-                            <p>{description}</p>
-                        </div>
-                    </div>
+const Card = ({title, description, img, leftColor, rightColor}) => (
+    <div className="card mx-auto Fitness-Card"
+    style={{
+        backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor || '#56CCF2'}, ${rightColor || '#2F80ED'})`
+    }}
+    >
+        <div className="card-body">
+            <div className="row center">
+                <div className="col-6">
+                    <img src={img || emptyImg} className="float-end" alt="img"/>
+                </div>
+                <div className="col-6 Fitness-Card-Info">
+                    <h1>{title}</h1>
+                    <p>{description}</p>
                 </div>
             </div>
-        );
-    }
-
-}
+        </div>
+    </div>
+);
 
 export default Card;
--- src/components/ExerciseForm.js
+++ src/components/ExerciseForm.js
@@ -1,75 +1,67 @@
 import React from "react";
 
-class ExercisesForm extends React.Component {
-
-    render(){
-        
-        const { onChange, onSubmit, form } = this.props;
-
-        return (
-            <div className="container">
-                <form onSubmit={onSubmit}>
-                    <div className="mb-3 mt-3">
-                        <input 
-                            className="form-control"
-                            placeholder="Title"
-                            type="text"
-                            name="title"
-                            onChange={onChange}
-                            value={form.title || ''}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input 
-                            className="form-control"
-                            placeholder="Description"
-                            type="text"
-                            name="description"
-                            onChange={onChange}
-                            value={form.description || ''}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <input 
-                            className="form-control"
-                            placeholder="Image"
-                            type="text"
-                            name="img"
-                            onChange={onChange}
-                            value={form.img || ''}
-                        />
-                    </div>
-                    <div className="row mb-3">
-                        <div className="col">
-                            <input 
-                                className="form-control"
-                                placeholder="Left Color"
-                                type="text"
-                                name="leftColor"
-                                onChange={onChange}
-                                value={form.leftColor || ''}
-                            />
-                        </div>
-                        <div className="col">
-                            <input 
-                                className="form-control"
-                                placeholder="Right Color"
-                                type="text"
-                                name="rightColor"
-                                onChange={onChange}
-                                value={form.rightColor || ''}
-                            />
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <button className="btn btn-primary" type="submit">
-                            Submit
-                        </button>
-                    </div>
-                </form>
+const ExercisesForm = ({onChange, onSubmit, form}) => (
+    <div className="container">
+        <form onSubmit={onSubmit}>
+            <div className="mb-3 mt-3">
+                <input 
+                    className="form-control"
+                    placeholder="Title"
+                    type="text"
+                    name="title"
+                    onChange={onChange}
+                    value={form.title || ''}
+                />
             </div>
-        );
-    }
-}
+            <div className="mb-3">
+                <input 
+                    className="form-control"
+                    placeholder="Description"
+                    type="text"
+                    name="description"
+                    onChange={onChange}
+                    value={form.description || ''}
+                />
+            </div>
+            <div className="mb-3">
+                <input 
+                    className="form-control"
+                    placeholder="Image"
+                    type="text"
+                    name="img"
+                    onChange={onChange}
+                    value={form.img || ''}
+                />
+            </div>
+            <div className="row mb-3">
+                <div className="col">
+                    <input 
+                        className="form-control"
+                        placeholder="Left Color"
+                        type="text"
+                        name="leftColor"
+                        onChange={onChange}
+                        value={form.leftColor || ''}
+                    />
+                </div>
+                <div className="col">
+                    <input 
+                        className="form-control"
+                        placeholder="Right Color"
+                        type="text"
+                        name="rightColor"
+                        onChange={onChange}
+                        value={form.rightColor || ''}
+                    />
+                </div>
+            </div>
+            <div className="mb-3">
+                <button className="btn btn-primary" type="submit">
+                    Submit
+                </button>
+            </div>
+        </form>
+    </div>
+);
 
 export default ExercisesForm;
