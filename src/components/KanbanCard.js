410c33bfe95d7a58585a48b11c8257ee0484e97d
Hugo Rodríguez <hugsrodriguez8@gmail.com>
2022-08-12 02:45:51 -0600

    Fin de prueba

--- src/components/KanbanCard.js
+++ src/components/KanbanCard.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 import './styles/kanban_card.scss';
+import AddButton from "./AddButton";
 
 const KanbanCard = ({task}) => {
+
     return(
         <div className="card">
             {task.title}
